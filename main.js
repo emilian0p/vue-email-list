4a60300885672b11899cf95ce4da1596223dd83b
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-07-14 17:58:37 +0200

    fine + bonus

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ const app = new Vue({
 
     },
     methods: {
-        randomEmail: function () {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then( function (response) {
-                    console.log(response.data);
-                });
-            },
+
+    },
+    created: function randomEmail() {
+        for (let i = 0; i < 10; i++) {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            console.log(response.data.response);
+            this.emails.push(response.data.response);
+        });
+    };
     }
 });
-
